67dc5f88d86f21c0ecce152f6570fc0524f0eb49
Keito <neymar.keito11@gmail.com>
2023-02-24 02:30:35 +0900

    新規投稿

--- app/controllers/snows_controller.rb
+++ app/controllers/snows_controller.rb
@@ -1,2 +1,21 @@
 class SnowsController < ApplicationController
+
+    def new
+        @snow = Snow.new
+      end
+    
+      def create
+        snow = Snow.new(snow_params)
+        if snow.save!
+          redirect_to :action => "index"
+        else
+          redirect_to :action => "new"
+        end
+      end
+    
+      private
+      def snow_params
+        params.require(:snow).permit(:spot, :address, :image, :hp, :telephone)
+      end
+
 end
