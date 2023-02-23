f4420abf8c06cef4d96838c6a167baf430ae7416
NakatsugawaAkari <akaritakuto0128@gmail.com>
2023-02-24 05:16:38 +0900

    編集等入れた

--- app/controllers/snows_controller.rb
+++ app/controllers/snows_controller.rb
@@ -3,21 +3,43 @@ class SnowsController < ApplicationController
 
     def new
         @Snow = Snow.new
-      end
+    end
     
-      def create
-        snow = Snow.new(snow_params)
+    def create
+    snow = Snow.new(snow_params)
         if snow.save!
-          redirect_to :action => "index"
+            redirect_to :action => "index"
         else
-          redirect_to :action => "new"
+            redirect_to :action => "new"
         end
-      end
-    
-      def index
+    end
+
+    def index
         @snows = Snow.all
     end
 
+    def show
+        @Snow = Snow.find(params[:id])
+    end
+
+    def edit
+        @Snow = Snow.find(params[:id])
+    end
+
+    def update
+        snow = Snow.find(params[:id])
+        if snow.update(snow_params)
+          redirect_to :action => "show", :id => snow.id
+        else
+          redirect_to :action => "new"
+        end
+    end
+
+    def destroy
+        snow = Snow.find(params[:id])
+        snow.destroy
+        redirect_to action: :index
+    end
 
       private
       def snow_params
