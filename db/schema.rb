6d88232316ac518e61756149e5f5182593101d5d
NakatsugawaAkari <akaritakuto0128@gmail.com>
2023-04-10 11:25:38 +0900

    user関連css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_03_193911) do
+ActiveRecord::Schema.define(version: 2023_04_07_152624) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -68,6 +68,9 @@ ActiveRecord::Schema.define(version: 2023_03_03_193911) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.string "name"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
